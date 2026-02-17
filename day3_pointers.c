b56f455465d9db4f729d249a1be3fda9cb052efb
chithra-17 <chithsd144@gmail.com>
2026-02-17 16:34:55 +0530

    added pointer program

---
+++ day3_pointers.c
@@ -0,0 +1,12 @@
+//swap 2 number using pointers
+#include <stdio.h>
+void swap(int *x, int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main(){
+    int a=10;
+    int b=20;
+    swap(&a, &b);   
+}
