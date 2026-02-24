8c76dac77a7514778070cc30982af178bb81fce5
chithra-17 <chithsd144@gmail.com>
2026-02-24 22:29:50 +0530

    Implemented bubble sort program

---
+++ bubble_sort.c
@@ -0,0 +1,19 @@
+// buble sort
+#include<stdio.h>
+int main(){
+    int arr[5]={1,5,6,7,9};
+    int temp;
+
+    for (int i=0; i<5-1;i++){
+        for (int j=0;j<5-1-i;j++){
+            if(arr[j]<arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for (int i=0;i<5;i++){
+        printf("%d\n",arr[i]);
+    }
+}
